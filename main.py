0358538144b07424de04c41a104e150792ce005e
Derek-Omar-Gomez-Castillo <derek_omar_gomez_castillo@comunidad.unam.mx>
2024-02-29 18:59:59 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import pygame as pg, sys
+
+class Player(pg.sprite.Sprite):
+    def __init__(self, width, heigth, position_x, position_y, color):
+        super().__init__()
+        self.image = pg.Surface([width, heigth])
+        self.image.fill(color)
+        self.rect = self.image.get_rect
+        
+
+pg.init
+
+clock = pg.time.Clock()
+
+screen_width = 800
+screen_height = 800
+screen = pg.display.set_mode( (screen_width, screen_height) )
+
+player = Player(50, 50, 100, 100,(255, 255, 255))
+player_group = pg.sprite.Group()
+player_group.add(player)
+
+
+while True:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+            sys.exit()
+
+    pg.display.flip()
+    player_group.draw(screen)
+
+    clock.tick(60)
