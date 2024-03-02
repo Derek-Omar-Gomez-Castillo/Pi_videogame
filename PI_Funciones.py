98886ad87e00b7e02e4a6d866ee05a91061874dc
Derek-Omar-Gomez-Castillo <derek_omar_gomez_castillo@comunidad.unam.mx>
2024-03-01 22:26:58 -0600

    refactorizando

--- PI_Funciones.py
+++ PI_Funciones.py
@@ -1,6 +1,5 @@
 import math, random
 def metodo_poligonos(numero_lados_poligonos):
-
     num_lados_poligono = numero_lados_poligonos  # Número de lados del polígono
     radio_circunferencia = 1  # Este es el radio de la circunferencia; el valor de pi no se ve afectado.
 
@@ -23,30 +22,27 @@ def metodo_poligonos(numero_lados_poligonos):
     print("Dicho de otra manera, el valor de pi se encuentra entre")
     print(pi + error,  "   y   ", pi - error )
 
-def metodo_monte_carlo():
-    num_puntos = 1000000
+def metodo_monte_carlo(numero_de_puntos):
     num_puntos_circulo = 0
     radio_circunferencia = 1
-    for i in range(num_puntos):
+    for i in range(numero_de_puntos):
         x = random.uniform(-1, 1)
         y = random.uniform(-1, 1)
         if x**2 + y**2 <= radio_circunferencia**2:
             num_puntos_circulo += 1
-    pi = 4 * num_puntos_circulo / num_puntos
-    print("Con ", num_puntos, " puntos, obtenemos:")
+    pi = 4 * num_puntos_circulo / numero_de_puntos
+    print("Con ", numero_de_puntos, " puntos, obtenemos:")
     print("Pi = ", pi)
     
 
-def metodo_brasilea():
+def metodo_basilea(numero_de_terminos):
     denominador = 1
     valores_fracciones = []
-    suma = 0
-    for i in range(1, 100000000):
+    suma_inversos_positivos_al_cuadrado = 0
+    for i in range(1, numero_de_terminos + 1):
         denominador = i ** 2
         valores_fracciones.append(1/denominador)
 
-    for j in valores_fracciones:
-        suma += j
-    pi = math.sqrt(6 * suma)
-    print(pi)
-metodo_brasilea()
+    for numero in valores_fracciones:
+        suma_inversos_positivos_al_cuadrado += numero
+    pi = math.sqrt(6 * suma_inversos_positivos_al_cuadrado)
