1be2c8a7f484e2e553b734f60f0df6fdd7378ce7
Derek-Omar-Gomez-Castillo <derek_omar_gomez_castillo@comunidad.unam.mx>
2024-03-03 22:20:47 -0600

    wazaaaaaaa

--- main.py
+++ main.py
@@ -13,21 +13,22 @@ class Player(Sprite):
 
 class Game_state():
     def __init__(self) :
-        self.state = 'Intro'
+        self.state = 'Montecarlo'
 
     def Intro(self):
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 pg.quit()
                 sys.exit()
-            if event.type == pg.KEYDOWN:
-                if event.key == pg.K_1:
-                     pass
-            if event.type == pg.KEYUP:
-                if event.key == pg.K_1:
-                    self.state = 'Montecarlo'
+            if event.type == pg.MOUSEBUTTONDOWN:
+                self.state = 'Montecarlo'
+
+        screen.blit(background_Intro, (0, 0))
+        #refactorizar
+        #player_group.draw(screen)
+        #player_group.update()
+        
         pg.display.flip()
-        screen.blit(background, (0,0))
 
     def Montecarlo(self):
         for event in pg.event.get():
@@ -35,9 +36,8 @@ class Game_state():
                 pg.quit()
                 sys.exit()
         
-        screen.blit(background, (0, 0))
-        player_group.draw(screen)
-        player_group.update()
+        screen.blit(heart_surface, (600, 300))
+        screen.blit(Tierra_image, (600, 300))
         pg.display.flip()
 
     def Basilea():
@@ -49,7 +49,7 @@ class Game_state():
     def State_manager(self):
         if self.state == 'Intro':
             self.Intro()
-        if self.state == 'Montercalo':
+        if self.state == 'Montecarlo':
             self.Montecarlo()
 
 
@@ -57,18 +57,20 @@ pg.init
 
 clock = pg.time.Clock()
 
-screen_width = 1366
+screen_width = 1360
 screen_height = 768
 screen = pg.display.set_mode( (screen_width, screen_height) )
 pg.display.set_caption("Las aventuras de Pi")
-pg.mouse.set_visible(False)
+#pg.mouse.set_visible(False)
 
-background = pg.image.load("assets/city.png")
+background_Intro = pg.image.load("assets/city.png")
+Tierra_image = pg.image.load("assets/Tierra_image.png")
 player = Player("assets/player/Bot Wheel/charge.png")
 player_group = pg.sprite.Group()
 player_group.add(player)
 game_state = Game_state()
-
+heart_surface = pg.Surface([100,100])
+heart_surface.fill((255,255,255))
 
 while True:
     game_state.State_manager()
